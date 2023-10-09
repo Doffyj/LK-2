1eae58c5e2256603644f49fccfb28c31012712f1
Wciv <sanchezeguiajon@gmail.com>
2023-10-09 04:10:35 +0200

    Atv 1 realizada

---
+++ M06_Desarrollo web en entorno cliente/1/ejercicio3.js
@@ -0,0 +1,14 @@
+let arraynums=[1,2,3,4,5];
+
+for (i = 0; i < arraynums.length; i++)
+	console.log(arraynums[i]);
+
+function factorial(n){
+	let fac = 1;
+	if (n == 0 | n == 1)
+		return fac;
+	else if (n > 1){
+		
+	}
+
+}
